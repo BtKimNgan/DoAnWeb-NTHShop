66dca22fe77534b447f2775c2df94428950d4247
Ngân Bùi <btkimngan0807@gmail.com>
2022-06-16 16:39:18 +0700

    Add project files.

---
+++ DoAnWeb1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAnWeb1.MvcApplication" Language="C#" %>
---
+++ DoAnWeb1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAnWeb1.Startup))]
+namespace DoAnWeb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
